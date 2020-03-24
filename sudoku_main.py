5af8f743566cf4fcefd9e073c7d7ddc595c4e62e
ygsamueljeong <46389189+ygsamueljeong@users.noreply.github.com>
2020-03-24 15:32:05 +0900

    Add files via upload

--- sudoku_main.py
+++ sudoku_main.py
@@ -7,63 +7,76 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
 class Sudoku:
-    def __init__(self, row, col, dimension, screen):
-        self.row = row
-        self.col = col
-        self.dimension = dimension
+    def __init__(self, screen):
+        self.row = 9
+        self.col = 9
+        self.dimension = 500
         self.screen = screen
-        self.board = erase_cells(create_board())
-        self.puzzle = [[self.board[i][j] for i in range(self.row)] for j in range(self.col)]
-        self.practice_sheet = [[self.board[i][j] for i in range(self.row)] for j in range(self.col)]
+        self.control = erase_cells(create_board())
+        self.puzzle = [[self.control[row][col] for row in range(self.row)] for col in range(self.col)]
+        self.practice_sheet = [[self.control[row][col] for row in range(self.row)] for col in range(self.col)]
 
-    def visualize_puzzle(self):
-        # Visualizing the Board
-        for pos in range(0, 10):
-            if pos % 3 == 0:
+    def visualize_board(self):
+        # Refreshing the Screen
+        screen.fill(WHITE)
+        screen.blit(background, (0, 0))
+        screen.blit(title_text, ((70, 20), (500, 20)))
+        screen.blit(sub_text, ((660, 570), (660, 570)))
+
+        # Drawing Borders
+        for index in range(0, 10):
+            if index % 3 == 0:
                 thick = 3
             else:
                 thick = 1
-            pygame.draw.line(self.screen, WHITE, (150, pos*55.56+80), (self.dimension+150, pos*55.56+80), thick)
-            pygame.draw.line(self.screen, WHITE, (pos*55.56+150, 80), (pos*55.56+150, self.dimension+80), thick)
-        # Visualizing Cell Values
+            pygame.draw.line(self.screen, WHITE, (150, 80 + index * 55.56), (150 + self.dimension, 80 + index * 55.56), thick)
+            pygame.draw.line(self.screen, WHITE, (150 + index * 55.56, 80), (150 + index * 55.56, 80 + self.dimension), thick)
+
+        # Drawing Cell Values
         font = pygame.font.SysFont("Papyrus", 30, bold=True)
-        for row in range(0, self.row):
-            for col in range(0, self.col):
+        for row in range(self.row):
+            for col in range(self.col):
                 if self.puzzle[row][col] == ' ':
                     color = GREEN
                 else:
                     color = WHITE
                 text = font.render(str(self.practice_sheet[row][col]), 1, color)
-                screen.blit(text, ((170+row*55, col*55.56+85), (170+row*55.56, col*55.56+85)))
+                screen.blit(text, ((170 + row * 55.56, 85 + col * 55.56), (170 + row * 55.56, 85 + col * 55.56)))
         return
 
     def redraw_cell(self, value, row, col, color):
+        # Redrawing Incorrect Guess Values with new Guesses
         font = pygame.font.SysFont("Papyrus", 30, bold=True)
         text = font.render(str(value), 1, color)
-        screen.blit(text, ((170 + row * 55, col * 55.56 + 85), (170 + row * 55, col * 55.56 + 85)))
+        screen.blit(text, ((170 + row * 55.56, 85 + col * 55.56), (170 + row * 55.56, 85 + col * 55.56)))
         return
 
-    def visualize_solve_sudoku(self):
+    def visualize_solving(self):
+        # Exact Same Algo as 'solve_sudoku()' with Pygame Visualization Function
         for row in range(len(self.practice_sheet)):
             for column in range(len(self.practice_sheet)):
                 if self.practice_sheet[row][column] == ' ':
                     for guess in range(1, 10):
                         if check_guess(guess, row, column, self.practice_sheet):
+                            # Refreshing Screen to Prevent Overlap-Printing
+                            self.visualize_board()
+                            pygame.draw.rect(self.screen, RED, (150 + row * 55.56, 80 + column * 55.56, 55.56, 55.56), 5)
+                            pygame.display.update()
+                            pygame.time.delay(50)
+                            # Visualizing Guess
                             self.practice_sheet[row][column] = guess
                             self.redraw_cell(guess, row, column, GREEN)
                             pygame.display.update()
-                            pygame.time.delay(120)
-                            if self.visualize_solve_sudoku():
+                            pygame.time.delay(50)
+                            # Backtracking Recursion Implementation
+                            if self.visualize_solving():
                                 return True
                             self.practice_sheet[row][column] = ' '
-                            # Refreshing Screen to Prevent Overlap Printing
-                            screen.fill(WHITE)
-                            screen.blit(background, (0, 0))
-                            screen.blit(title_text, ((70, 20), (500, 20)))
-                            screen.blit(sub_text, ((660, 570), (660, 570)))
-                            self.visualize_puzzle()
+                            # Visualizing Backtracking Recursion
+                            self.visualize_board()
+                            pygame.draw.rect(self.screen, RED, (150 + row * 55.56, 80 + column * 55.56, 55.56, 55.56), 5)
                             pygame.display.update()
-                            pygame.time.delay(120)
+                            pygame.time.delay(50)
                     return False
         return True
 
@@ -78,12 +91,12 @@ if __name__ == '__main__':
     pygame.display.set_icon(icon)
 
     # Run Visualizer
-    board = Sudoku(9, 9, 500, screen)
+    my_board = Sudoku(screen)
     running = True
     while running:
         screen.fill(WHITE)
         screen.blit(background, (0, 0))
-        # Title Print
+        # Title
         font = pygame.font.SysFont("Papyrus", 35)
         title_text = font.render(str('Sudoku Backtracking Algorithm Visualizer'), 1, WHITE)
         screen.blit(title_text, ((70, 20), (500, 20)))
@@ -92,12 +105,13 @@ if __name__ == '__main__':
         sub_text = font.render(str('Press SPACE to Solve!'), 1, WHITE)
         screen.blit(sub_text, ((655, 570), (655, 570)))
         # Board Print
-        board.visualize_puzzle()
+        my_board.visualize_board()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYUP:
+                # Press SPACE to Solve Sudoku
                 if event.key == pygame.K_SPACE:
-                    board.visualize_solve_sudoku()
+                    my_board.visualize_solving()
         pygame.display.update()
 
